94b6dd4b59918074d864562ea865ba54f58137cd
kirti1602 <kirtikashyap9a@gmail.com>
2025-02-24 14:16:18 +0530

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,5 @@
+# Create a new set of identical items from two sets
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+common_items = set1 & set2  # Using & operator for intersection
+print(common_items)  # Output: {4, 5}
